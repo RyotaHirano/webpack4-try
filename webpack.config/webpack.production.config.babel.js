26a905e1216991902a3ba5e42d5ee20eee46d9ba
ryotahirano <hirano@tsumikiinc.com>
2018-02-26 19:07:11 +0900

    upgrade loaders

--- webpack.config/webpack.production.config.babel.js
+++ webpack.config/webpack.production.config.babel.js
@@ -12,39 +12,5 @@ const plugins = [
 
 module.exports = merge(baseConfig, {
   mode: 'production',
-  plugins,
-  module: {
-    rules: [
-      {
-        test: /\.(jpe?g|png|svg|gif|ico)$/,
-        use:
-          [
-            {
-              loader: 'file-loader',
-              options: {
-                name: 'assets/[path][name].[ext]'
-              }
-            },
-            {
-              loader: 'image-webpack-loader',
-              query: {
-                mozjpeg: {
-                  progressive: true,
-                },
-                gifsicle: {
-                  interlaced: false,
-                },
-                optipng: {
-                  optimizationLevel: 7,
-                },
-                pngquant: {
-                  quality: '65-90',
-                  speed: 4,
-                },
-              }
-            }
-          ]
-      }
-    ]
-  }
+  plugins
 })
