5f573ac972451d1e38788c6f8ee7a03bf84b5b36
ryotahirano <hirano@tsumikiinc.com>
2018-05-14 12:59:50 +0900

    replace webpack-dev-server with webpack-serve

--- webpack.config/webpack.base.config.babel.js
+++ webpack.config/webpack.base.config.babel.js
@@ -1,10 +1,8 @@
 const path = require('path')
-const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 const plugins = [
-  new webpack.HotModuleReplacementPlugin(),
   new HtmlWebpackPlugin({
     template: `src/html/index.pug`
   })
--- webpack.config/webpack.development.config.babel.js
+++ webpack.config/webpack.development.config.babel.js
@@ -1,6 +1,6 @@
 const merge = require('webpack-merge')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
-import { resolve } from 'path'
+const resolve = require('path').resolve
 const rootResolve = pathname => resolve(__dirname, pathname)
 
 const baseConfig = require('./webpack.base.config.babel.js')
@@ -14,10 +14,8 @@ const plugins = [
 module.exports = merge(baseConfig, {
   mode: 'development',
   plugins,
-  devServer: {
-    contentBase: rootResolve('dist'),
-    publicPath: '/',
-    inline: true,
+  serve: {
+    content: rootResolve('dist'),
     hot: true,
     host: '0.0.0.0'
   }
